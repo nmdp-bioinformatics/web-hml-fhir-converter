4bf2a3efba5f2f436ece4c6a90e69ce6bb47bf8d
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2016-12-08 11:09:01 -0600

    Working with File API.

--- app/scripts/controllers/file/file.js
+++ app/scripts/controllers/file/file.js
@@ -4,14 +4,13 @@
 (function () {
     'use strict';
 
-    angular.module('hmlFhirAngularClientApp.controllers').controller('file', file);
-    file.$inject = ['$scope', '$uibModalInstance', 'title'];
-
     function file ($scope, $uibModalInstance, title) {
         var fileCtrl = this;
 
+        fileCtrl.scope = $scope;
         fileCtrl.title = title;
         fileCtrl.mode = 1; // this will set it to 'upload' mode.
+        fileCtrl.files = [];
 
         fileCtrl.close = function () {
             $uibModalInstance.close(true);
@@ -25,8 +24,15 @@
             if (fileCtrl.mode > 1) {
                 fileCtrl.mode = 1;
             } else {
-                fileCtrl.mode = 2;
+                fileCtrl.mode = 2;  // This sets it to 'instruction' mode.
             }
         };
+
+        $scope.fileInputChange = function (element) {
+            fileCtrl.files = element.files;
+        };
     }
+
+    angular.module('hmlFhirAngularClientApp.controllers').controller('file', file);
+    file.$inject = ['$scope', '$uibModalInstance', 'title'];
 }());
