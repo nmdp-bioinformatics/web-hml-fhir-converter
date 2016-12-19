c7848a00ab6719a1a39d9d32651dfece9f3ed076
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2016-12-19 14:02:36 -0600

    Typing test name addition.

---
+++ app/scripts/controllers/guided/hml/typing-test-names/typing-test-name-add-edit.js
@@ -0,0 +1,43 @@
+/**
+ * Created by abrown3 on 12/19/16.
+ */
+(function () {
+    'use strict';
+
+    function typingTestNameAddEdit ($scope, $uibModalInstance, typingTestId, edit, typingTestName) {
+        /* jshint validthis: true */
+        var typingTestNameAddEditCtrl = this;
+
+        typingTestNameAddEditCtrl.scope = $scope;
+        typingTestNameAddEditCtrl.formSubmitted = false;
+        typingTestNameAddEditCtrl.typingTestName = {
+            id: typingTestId,
+            name: undefined,
+            init: true
+        };
+
+        if (edit) {
+            typingTestNameAddEditCtrl.typingTestName = typingTestName;
+        }
+
+        typingTestNameAddEditCtrl.cancel = function () {
+            $uibModalInstance.dismiss();
+        };
+
+        typingTestNameAddEditCtrl.close = function () {
+            $uibModalInstance.close();
+        };
+
+        typingTestNameAddEditCtrl.add = function (form) {
+            typingTestNamesCreateCtrl.formSubmitted = true;
+
+            if (!form.$invalid) {
+                typingTestNameAddEditCtrl.formSubmitted = false;
+                $uibModalInstance.close(typingTestNameAddEditCtrl.typingTestName);
+            }
+        };
+    }
+
+    angular.module('hmlFhirAngularClientApp.controllers').controller('typingTestNameAddEdit', typingTestNameAddEdit);
+    typingTestNameAddEdit.$inject = ['$scope', '$uibModalInstance', 'typingTestId', 'edit', 'typingTestName'];
+}());
--- app/scripts/controllers/guided/hml/typing-test-names/typing-test-names.js
+++ app/scripts/controllers/guided/hml/typing-test-names/typing-test-names.js
@@ -4,13 +4,75 @@
 (function () {
     'use strict';
 
-    function typingTestNames ($scope) {
+    function typingTestNames ($scope, guidGenerator, $uibModal) {
         /* jshint validthis: true */
-        var typingTestNamesCtrl = this;
+        var typingTestNamesCtrl = this,
+            parentCtrl = $scope.hmlModalCtrl;
 
         typingTestNamesCtrl.scope = $scope;
+        typingTestNamesCtrl.edit = parentCtrl.edit;
+        typingTestNamesCtrl.typingTestNames = [];
+
+        if (typingTestNamesCtrl.edit) {
+            typingTestNamesCtrl.typingTestNames = parentCtrl.hml.typingTestNames;
+        }
+
+        $scope.$on('guided:hml:node:update', function () {
+            $scope.$emit('guided:hml:node:updated', typingTestNamesCtrl.typingTestNames);
+        });
+
+
+        typingTestNamesCtrl.editTypingTestNameEntry = function (typingTestName) {
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'views/guided/hml/typing-test-names/typing-test-name-add-edit.html',
+                controller: 'typingTestNamesCreate',
+                controllerAs: 'typingTestNamesCreateCtrl',
+                resolve: {
+                    typingTestId: function () {
+                        return typingTestName.id;
+                    },
+                    edit: function () {
+                        return true;
+                    },
+                    typingTestName: function () {
+                        return typingTestName;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (typingTestName) {
+                if (typingTestName) {
+                    updateTypingTestName(typingTestName);
+                }
+            });
+        };
+
+        typingTestNamesCtrl.removeTypingTestName = function (typingTestName) {
+            var index = getTypingTestNameIndex(typingTestName);
+            typingTestNamesCtrl.typingTestNames.slice(index, 1);
+        };
+
+        function getTypingTestNameIndex (typingTestName) {
+            for (var i = 0; i < typingTestNamesCtrl.typingTestNames.length; i++) {
+                if (typingTestNamesCtrl.typingTestNames[i].id === typingTestName.id) {
+                    return i;
+                }
+            }
+
+            return undefined;
+        }
+
+        function updateTypingTestName (typingTestName) {
+            for (var i = 0; i < typingTestNamesCtrl.typingTestNames.length; i++) {
+                if (typingTestNamesCtrl.typingTestNames[i].id === typingTestName.id) {
+                    typingTestNamesCtrl.typingTestNames[i] = typingTestName;
+                    return;
+                }
+            }
+        }
     }
 
     angular.module('hmlFhirAngularClientApp.controllers').controller('typingTestNames', typingTestNames);
-    typingTestNames.$inject = ['$scope'];
+    typingTestNames.$inject = ['$scope', 'guidGenerator', '$uibModal'];
 }());
