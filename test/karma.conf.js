6a13cee28dc70627bc020170eadd562bde57fdf2
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2017-01-04 16:13:02 -0600

    Add angular-ui-grid.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
       'bower_components/AngularJS-Toaster/toaster.js',
       'bower_components/x2js/xml2json.min.js',
       'bower_components/angular-x2js/dist/x2js.min.js',
+      'bower_components/angular-ui-grid/ui-grid.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
