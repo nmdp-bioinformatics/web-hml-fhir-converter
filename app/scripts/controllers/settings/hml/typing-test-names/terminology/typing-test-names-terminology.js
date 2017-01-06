835f604d809dd40c8370930b636b2caea3e6dc4a
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2017-01-05 22:15:40 -0600

    Finessing grid and columns.

--- app/scripts/controllers/settings/hml/typing-test-names/terminology/typing-test-names-terminology.js
+++ app/scripts/controllers/settings/hml/typing-test-names/terminology/typing-test-names-terminology.js
@@ -6,14 +6,34 @@
 
     function typingTestNamesTerminology($scope, typingTestNameService) {
         /* jshint validthis: true */
-        var typingTestNamesTerminologyCtrl = this;
+        var typingTestNamesTerminologyCtrl = this,
+            activeColumnTemplate =
+                '<div class="ui-grid-cell-contents">' +
+                    '<div class="row">' +
+                        '<div class="col-md-12">' +
+                            '<input type="checkbox" data-ng-click="typingTestNamesTerminologyCtrl.activateItem(this)" />'
+                        '</div>' +
+                    '</div>' +
+                '</div>';
 
         typingTestNamesTerminologyCtrl.scope = $scope;
-        typingTestNamesTerminologyCtrl.typingTestNames = [];
         typingTestNamesTerminologyCtrl.maxQuery = 10;
+        typingTestNamesTerminologyCtrl.gridOptions = {
+            data: [],
+            enableSorting: true,
+            showGridFooter: true,
+            enableCellEditOnFocus: true,
+            columnDefs: [
+                { name: 'id', field: 'id', visible: false },
+                { name: 'name', field: 'name', displayName: 'Name:', enableCellEdit: true },
+                { name: 'description', field: 'description', displayName: 'Description:' },
+                { name: 'dateCreated', field: 'dateCreated', displayName: 'Date Created:' },
+                { name: 'active', field: 'active', displayName: '', maxWidth: 30, enableColumnMenu: false }
+            ]
+        };
 
         typingTestNameService.getTypingTestNameTerminology(typingTestNamesTerminologyCtrl.maxQuery).then(function (typingTestNames) {
-            typingTestNamesTerminologyCtrl.typingTestNames = typingTestNames;
+            typingTestNamesTerminologyCtrl.gridOptions.data = typingTestNames;
         });
     }
 
