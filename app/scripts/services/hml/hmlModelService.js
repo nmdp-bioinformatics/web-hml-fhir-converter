23c4be5f06fd612566941589a6174b38107b638c
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2017-02-21 16:23:05 -0600

    Modify get model service

--- app/scripts/services/hml/hmlModelService.js
+++ app/scripts/services/hml/hmlModelService.js
@@ -9,48 +9,8 @@
             getModel: function (modelName) {
                 var url = '',
                     defer = $q.defer(),
-                    headers = httpHeaderTransform.getHeaderForResourceServer();
-
-                switch (modelName) {
-                    case 'CollectionMethod':
-                        url = getCollectionMethodModelUrl();
-                        break;
-                    case 'ExtendedItem':
-                        url = getExtendedItemModelUrl();
-                        break;
-                    case 'Hml':
-                        url = getHmlModelUrl();
-                        break;
-                    case 'HmlId':
-                        url = getHmlIdModelUrl();
-                        break;
-                    case 'Project':
-                        url = getProjectModelUrl();
-                        break;
-                    case 'Property':
-                        url = getPropertyModelUrl();
-                        break;
-                    case 'ReportingCenter':
-                        url = getReportingCenterModelUrl();
-                        break;
-                    case 'Sample':
-                        url = getSampleModelUrl();
-                        break;
-                    case 'TypingTestName':
-                        url = getTypingTestNameModelUrl();
-                        break;
-                    case 'Version':
-                        url = getVersionModelUrl();
-                        break;
-                    case 'Typing':
-                        url = getTypingModelUrl();
-                        break;
-                    case 'TypingMethod':
-                        url = getTypingMethodModelUrl();
-                        break;
-                    default:
-                        return;
-                }
+                    headers = httpHeaderTransform.getHeaderForResourceServer(),
+                    url = getUrl(modelName);
 
                 $http({
                    method: 'GET',
@@ -64,52 +24,9 @@
             }
         };
 
-        function getCollectionMethodModelUrl () {
-            return appConfig.resource_server_base_url + 'collectionMethod';
-        }
-
-        function getExtendedItemModelUrl () {
-            return appConfig.resource_server_base_url + 'extendedItem';
-        }
-
-        function getHmlModelUrl () {
-            return appConfig.resource_server_base_url + 'hml';
-        }
-
-        function getHmlIdModelUrl () {
-            return appConfig.resource_server_base_url + 'hmlId';
-        }
-
-        function getProjectModelUrl () {
-            return appConfig.resource_server_base_url + 'project';
-        }
-
-        function getPropertyModelUrl () {
-            return appConfig.resource_server_base_url + 'property';
-        }
-
-        function getReportingCenterModelUrl () {
-            return appConfig.resource_server_base_url + 'reportingCenter';
-        }
-
-        function getSampleModelUrl () {
-            return appConfig.resource_server_base_url + 'sample';
-        }
-
-        function getTypingTestNameModelUrl () {
-            return appConfig.resource_server_base_url + 'typingTestName';
-        }
-
-        function getVersionModelUrl() {
-            return appConfig.resource_server_base_url + 'version';
-        }
-
-        function getTypingModelUrl() {
-            return appConfig.resource_server_base_url + 'typing';
-        }
-
-        function getTypingMethodModelUrl() {
-            return appConfig.resource_server_base_url + 'typingMethod';
+        function getUrl(modelName) {
+            var resource = modelName.charAt(0).toLowerCase() + modelName.substr(1, modelName.length);
+            return appConfig.resource_server_base_url + resource;
         }
 
         return service;
