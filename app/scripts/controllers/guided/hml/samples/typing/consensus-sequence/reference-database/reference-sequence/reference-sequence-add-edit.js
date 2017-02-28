f54efc026435d2cfb463255ab6926693282a997c
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2017-02-28 11:36:16 -0600

    Updates

--- app/scripts/controllers/guided/hml/samples/typing/consensus-sequence/reference-database/reference-sequence/reference-sequence-add-edit.js
+++ app/scripts/controllers/guided/hml/samples/typing/consensus-sequence/reference-database/reference-sequence/reference-sequence-add-edit.js
@@ -4,13 +4,47 @@
 (function () {
     'use strict';
 
-    function referenceSequenceAddEdit ($scope) {
+    function referenceSequenceAddEdit ($scope, $uibModalInstance, referenceSequence, hmlModel, parentCollectionPropertyAllocation, usSpinnerService) {
         /* jshint validthis:true */
         var referenceSequenceAddEditCtrl = this;
 
+        $scope.parentCtrl = referenceSequenceAddEditCtrl;
+        usSpinnerService.stop('index-spinner');
+
         referenceSequenceAddEditCtrl.scope = $scope;
+        referenceSequenceAddEditCtrl.hml = hmlModel;
+        referenceSequenceAddEditCtrl.formSubmitted = false;
+        referenceSequenceAddEditCtrl.parentCollectionPropertyAllocation = parentCollectionPropertyAllocation;
+        referenceSequenceAddEditCtrl.sampleIndex = getSampleIndex(referenceSequenceAddEditCtrl.parentCollectionPropertyAllocation);
+        referenceSequenceAddEditCtrl.referenceSequence = referenceSequence;
+
+        referenceSequenceAddEditCtrl.cancel = function () {
+            $uibModalInstance.dismiss();
+        };
+
+        referenceSequenceAddEditCtrl.close = function () {
+            $uibModalInstance.close();
+        };
+
+        referenceSequenceAddEditCtrl.add = function (form) {
+            referenceSequenceAddEditCtrl.formSubmitted = true;
+
+            if (!form.$invalid) {
+                referenceSequenceAddEditCtrl.formSubmitted = false;
+                $uibModalInstance.close(referenceSequenceAddEditCtrl.referenceSequence);
+            }
+        };
+
+        function getSampleIndex (propertyMap) {
+            var isEqual = function (item) {
+                    return item.propertyString === 'samples';
+                },
+                index = R.findIndex(isEqual, propertyMap);
+
+            return propertyMap[index].propertyIndex;
+        }
     }
 
     angular.module('hmlFhirAngularClientApp.controllers').controller('referenceSequenceAddEdit', referenceSequenceAddEdit);
-    referenceSequenceAddEdit.$inject = ['$scope'];
+    referenceSequenceAddEdit.$inject = ['$scope', '$uibModalInstance', 'referenceSequence', 'hmlModel', 'parentCollectionPropertyAllocation', 'usSpinnerService'];
 }());
--- app/scripts/controllers/guided/hml/samples/typing/consensus-sequence/reference-database/reference-sequence/reference-sequence.js
+++ app/scripts/controllers/guided/hml/samples/typing/consensus-sequence/reference-database/reference-sequence/reference-sequence.js
@@ -4,13 +4,59 @@
 (function () {
     'use strict';
 
-    function referenceSequence ($scope) {
+    function referenceSequence ($scope, $uibModal, objectModelFactory, usSpinnerService) {
         /* jshint validthis:true */
-        var referenceSequenceCtrl = this;
+        var referenceSequenceCtrl = this,
+            parentCtrl = $scope.parentCtrl;
+
+        usSpinnerService.stop('index-spinner');
 
         referenceSequenceCtrl.scope = $scope;
+        referenceSequenceCtrl.hml = parentCtrl.hml;
+        referenceSequenceCtrl.sampleIndex = parentCtrl.sampleIndex;
+        referenceSequenceCtrl.parentCollectionPropertyAllocation = returnPropertyLocator();
+
+        referenceSequenceCtrl.addReferenceSequence = function () {
+            usSpinnerService.spin('index-spinner');
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'views/guided/hml/samples/typing/consensus-sequence/reference-database/reference-sequence/reference-sequence-add-edit.html',
+                controller: 'referenceSequence',
+                controllerAs: 'referenceSequenceCtrl',
+                resolve: {
+                    referenceSequence: function () {
+                        return objectModelFactory.getModelByName('ReferenceSequence');
+                    },
+                    hmlModel: function () {
+                        return referenceSequenceCtrl.hml;
+                    },
+                    parentCollectionPropertyAllocation: function () {
+                        return referenceSequenceCtrl.parentCollectionPropertyAllocation;
+                    },
+                    edit: function () {
+                        return false;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (result) {
+                if (result) {
+
+                }
+            });
+        };
+
+        function returnPropertyLocator () {
+            return [
+                { propertyString: 'samples', propertyIndex: referenceSequenceCtrl.sampleIndex, isArray: true },
+                { propertyString: 'typing', propertyIndex: -1, isArray: false },
+                { propertyString: 'consensusSequence', propertyIndex: -1, isArray: false },
+                { propertyString: 'referenceDatabase', propertyIndex: -1, isArray: false },
+                { propertyString: 'referenceSequence', propertyIndex: -1, isArray: false }
+            ];
+        }
     }
 
     angular.module('hmlFhirAngularClientApp.controllers').controller('referenceSequence', referenceSequence);
-    referenceSequence.$inject = ['$scope'];
+    referenceSequence.$inject = ['$scope', '$uibModal', 'objectModelFactory', 'usSpinnerService'];
 }());
