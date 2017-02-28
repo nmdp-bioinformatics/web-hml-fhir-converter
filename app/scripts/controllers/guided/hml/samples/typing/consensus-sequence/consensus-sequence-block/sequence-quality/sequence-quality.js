bbf4a2f00be095825582e87a17f5f5600f4939bb
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2017-02-28 14:48:10 -0600

    Updates

--- app/scripts/controllers/guided/hml/samples/typing/consensus-sequence/consensus-sequence-block/sequence-quality/sequence-quality.js
+++ app/scripts/controllers/guided/hml/samples/typing/consensus-sequence/consensus-sequence-block/sequence-quality/sequence-quality.js
@@ -4,13 +4,59 @@
 (function () {
     'use strict';
 
-    function sequenceQuality ($scope) {
+    function sequenceQuality ($scope, $uibModal, objectModelFactory, usSpinnerService) {
         /* jshint validthis:true */
-        var sequenceQualityCtrl = this;
+        var sequenceQualityCtrl = this,
+            parentCtrl = $scope.parentCtrl;
+
+        usSpinnerService.stop('index-spinner');
 
         sequenceQualityCtrl.scope = $scope;
+        sequenceQualityCtrl.hml = parentCtrl.hml;
+        sequenceQualityCtrl.sampleIndex = parentCtrl.sampleIndex;
+        sequenceQualityCtrl.parentCollectionPropertyAllocation = returnPropertyLocator();
+
+        sequenceQualityCtrl.addSequenceQuality = function () {
+            usSpinnerService.spin('index-spinner');
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'views/guided/hml/samples/typing/consensus-sequence/consensus-sequence-block/sequence-quality/sequence-quality-add-edit.html',
+                controller: 'sequenceQualityAddEdit',
+                controllerAs: 'sequenceQualityAddEditCtrl',
+                resolve: {
+                    sequenceQuality: function () {
+                        return objectModelFactory.getModelByName('SequenceQuality');
+                    },
+                    hmlModel: function () {
+                        return sequenceQualityCtrl.hml;
+                    },
+                    parentCollectionPropertyAllocation: function () {
+                        return sequenceQualityCtrl.parentCollectionPropertyAllocation;
+                    },
+                    edit: function () {
+                        return false;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (result) {
+                if (result) {
+
+                }
+            });
+        };
+
+        function returnPropertyLocator () {
+            return [
+                { propertyString: 'samples', propertyIndex: sequenceQualityCtrl.sampleIndex, isArray: true },
+                { propertyString: 'typing', propertyIndex: -1, isArray: false },
+                { propertyString: 'consensusSequence', propertyIndex: -1, isArray: false },
+                { propertyString: 'consensusSequenceBlock', propertyIndex: -1, isArray: false },
+                { propertyString: 'sequenceQuality', propertyIndex: -1, isArray: false }
+            ];
+        }
     }
 
     angular.module('hmlFhirAngularClientApp.controllers').controller('sequenceQuality', sequenceQuality);
-    sequenceQuality.$inject = ['$scope'];
+    sequenceQuality.$inject = ['$scope', '$uibModal', 'objectModelFactory', 'usSpinnerService'];
 }());
--- app/scripts/controllers/guided/hml/samples/typing/consensus-sequence/consensus-sequence-block/variant/variant.js
+++ app/scripts/controllers/guided/hml/samples/typing/consensus-sequence/consensus-sequence-block/variant/variant.js
@@ -4,13 +4,59 @@
 (function () {
     'use strict';
 
-    function variant ($scope) {
+    function variant ($scope, $uibModal, objectModelFactory, usSpinnerService) {
         /* jshint validthis:true */
-        var variantCtrl = this;
+        var variantCtrl = this,
+            parentCtrl = $scope.parentCtrl;
+
+        usSpinnerService.stop('index-spinner');
 
         variantCtrl.scope = $scope;
+        variantCtrl.hml = parentCtrl.hml;
+        variantCtrl.sampleIndex = parentCtrl.sampleIndex;
+        variantCtrl.parentCollectionPropertyAllocation = returnPropertyLocator();
+
+        variantCtrl.addVariant = function () {
+            usSpinnerService.spin('index-spinner');
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'views/guided/hml/samples/typing/consensus-sequence/consensus-sequence-block/variant/variant-add-edit.html',
+                controller: 'variantAddEdit',
+                controllerAs: 'variantAddEditCtrl',
+                resolve: {
+                    variant: function () {
+                        return objectModelFactory.getModelByName('Variant');
+                    },
+                    hmlModel: function () {
+                        return variantCtrl.hml;
+                    },
+                    parentCollectionPropertyAllocation: function () {
+                        return variantCtrl.parentCollectionPropertyAllocation;
+                    },
+                    edit: function () {
+                        return false;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (result) {
+                if (result) {
+
+                }
+            });
+        };
+
+        function returnPropertyLocator () {
+            return [
+                { propertyString: 'samples', propertyIndex: variantCtrl.sampleIndex, isArray: true },
+                { propertyString: 'typing', propertyIndex: -1, isArray: false },
+                { propertyString: 'consensusSequence', propertyIndex: -1, isArray: false },
+                { propertyString: 'consensusSequenceBlock', propertyIndex: -1, isArray: false },
+                { propertyString: 'variant', propertyIndex: -1, isArray: false }
+            ];
+        }
     }
 
     angular.module('hmlFhirAngularClientApp.controllers').controller('variant', variant);
-    variant.$inject = ['$scope'];
+    variant.$inject = ['$scope', '$uibModal', 'objectModelFactory', 'usSpinnerService'];
 }());
