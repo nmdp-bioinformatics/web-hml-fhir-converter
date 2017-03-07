8280ab7f51f8462323be19ac9437426d36de7b11
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2017-03-07 11:45:53 -0600

    Updates

--- app/scripts/controllers/guided/hml/samples/typing/typing-method/sbt-ngs/raw-reads/raw-reads.js
+++ app/scripts/controllers/guided/hml/samples/typing/typing-method/sbt-ngs/raw-reads/raw-reads.js
@@ -4,13 +4,59 @@
 (function () {
     'use strict';
 
-    function rawReads ($scope) {
+    function rawReads ($scope, $uibModal, objectModelFactory, usSpinnerService) {
         /* jshint validthis:true */
-        var rawReadsCtrl = this;
+        var rawReadsCtrl = this,
+            parentCtrl = $scope.parentCtrl;
 
         rawReadsCtrl.scope = $scope;
+        rawReadsCtrl.hml = parentCtrl.hml;
+        rawReadsCtrl.sampleIndex = parentCtrl.sampleIndex;
+        rawReadsCtrl.parentCollectionPropertyAllocation = returnPropertyLocator();
+
+        usSpinnerService.stop('index-spinner');
+
+        rawReadsCtrl.addGssp = function () {
+            usSpinnerService.spin('index-spinner');
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'views/guided/hml/samples/typing/typing-method/sbt-ngs/raw-reads/raw-reads-add-edit.html',
+                controller: 'rawReadsAddEdit',
+                controllerAs: 'rawReadsAddEditCtrl',
+                resolve: {
+                    rawRead: function () {
+                        return objectModelFactory.getModelByName('RawRead');
+                    },
+                    hmlModel: function () {
+                        return rawReadsCtrl.hml;
+                    },
+                    parentCollectionPropertyAllocation: function () {
+                        return rawReadsCtrl.parentCollectionPropertyAllocation;
+                    },
+                    edit: function () {
+                        return false;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (result) {
+                if (result) {
+
+                }
+            });
+        };
+
+        function returnPropertyLocator () {
+            return [
+                { propertyString: 'samples', propertyIndex: rawReadsCtrl.sampleIndex, isArray: true },
+                { propertyString: 'typing', propertyIndex: -1, isArray: false },
+                { propertyString: 'typingMethod', propertyIndex: -1, isArray: false },
+                { propertyString: 'sbtSanger', propertyIndex: -1, isArray: false },
+                { propertyString: 'subAmplification', propertyIndex: -1, isArray: true }
+            ];
+        }
     }
 
     angular.module('hmlFhirAngularClientApp.controllers').controller('rawReads', rawReads);
-    rawReads.$inject = ['$scope'];
+    rawReads.$inject = ['$scope', '$uibModal', 'objectModelFactory', 'usSpinnerService'];
 }());
